af1a5f35a7496f265b19bf928594db47c83c459a
Shinya-Fukuta0116 <alleyezon.djwanted@gmail.com>
2023-01-22 05:59:33 +0900

    add time

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const RANDOM_QUOTE_API_URL = `https://api.quotable.io/random`
 const quoteDisplayElement = document.getElementById('quoteDisplay')
 const quoteInputElement = document.getElementById('quoteInput')
+const timerElement = document.getElementById('timer')
 
 quoteInputElement.addEventListener('input', () => {
   const arrayQuote = quoteDisplayElement.querySelectorAll('span')
@@ -40,7 +41,21 @@ async function renderNewQuote() {
     quoteDisplayElement.appendChild(characterSpan)
   });
   quoteInputElement.value = null
+  startTimer()
 };
+
+let startTime
+function startTimer() {
+  timerElement.innerText = 0
+  startTime = new Date()
+  setInterval(() => {
+    timer.innerText = getTimerTime()
+  }, 1000)
+}
+
+function getTimerTime() {
+  return Math.floor((new Date() - startTime) / 1000)
+}
 
 renderNewQuote()
 
